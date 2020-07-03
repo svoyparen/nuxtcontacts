7742db7427afdbd21d6add90dddc4d7569966d9f
svoyparen <62475107+svoyparen@users.noreply.github.com>
2020-07-03 21:45:55 +0300

    Add files via upload

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
+  plugins: ["~/plugins/vee-validate.js"
   ],
   /*
   ** Nuxt.js dev-modules
@@ -52,6 +52,7 @@ export default {
   ** Build configuration
   */
   build: {
+    transpile: ["vee-validate/dist/rules"],
     /*
     ** You can extend webpack config here
     */
