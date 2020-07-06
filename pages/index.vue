45e6d54d8d6178666b9e7a8d5cad2aacecd6c90f
svoyparen <62475107+svoyparen@users.noreply.github.com>
2020-07-06 23:53:01 +0300

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -3,55 +3,47 @@
 		<div class="form">
 			<h1>Написать письмо</h1>
 				<form id="post_form" @submit.prevent="submitForm">
-					<span class="send_message" v-if="send">Письмо успешно отправлено!</span>
-					<div class="input-form">
-						<label for="email">* Ваш email</label>
-						<!--<input type="text" placeholder="Email" v-model.trim="email" :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}">
-						<input type="email" placeholder="Email" v-model.trim="email" @blur="isIncorrectEmail">-->
-						<input type="email" placeholder="Email" v-model.trim="email">
-						<span class="helper-text invalid" v-if="submitted && !$v.email.required">Поле email не должно быть пустым</span>
-						<span class="helper-text invalid" v-if="submitted && !$v.email.email">Введите корректный email</span>
-						<span class="helper-text invalid" v-if="submitted && !$v.email.minLength">Поле email не должно быть меньше 8-и знаков</span>
-					</div>
-					<div class="input-form">
-						<label for="firstName">* Ваше имя</label>
-						<input type="text" placeholder="Имя" v-model.trim="firstName" id="firstName" name="firstName">
-						<span class="helper-text invalid" v-if="submitted && !$v.firstName.required">Поле Имя не должно быть пустым</span>
-						<span class="helper-text invalid" v-if="submitted && !$v.firstName.minLength">Поле Имя не должно быть меньше 3-х знаков</span>
-					</div>
-					<div class="input-form">
-						<label for="middleName">Отчество</label>
-						<input type="text" placeholder="Отчество" v-model.trim="middleName">
-						<span class="helper-text invalid" v-if="submitted && !$v.middleName.maxLength">Поле Отчество должно содержать не более 255-ти букв</span>
-					</div>
-					<div class="input-form">
-						<label for="lastName">Фамилия</label>
-						<input type="text" placeholder="Фамилия" v-model.trim="lastName">
-						<span class="helper-text invalid" v-if="submitted && !$v.lastName.maxLength">Поле Фамилия должно содержать не более 255-ти букв</span>
-					</div>
-					<div class="input-form">
-						<label for="phoneNumber">Телефон</label>
-						<input type="text" placeholder="Телефон" v-model.trim="phoneNumber">
-						<span class="helper-text invalid" v-if="submitted && !$v.phoneNumber.minLength">Поле Телефон должно содержать не меньше 11-ти цифр</span>
-						<span class="helper-text invalid" v-if="submitted && !$v.phoneNumber.maxLength">Поле Телефон должно содержать не больше 11-ти цифр</span>
-					</div>
-					<div class="input-form">
-						<label for="title">* Тема</label>
-						<input type="text" placeholder="Тема" v-model.trim="title">
-						<span class="helper-text invalid" v-if="submitted && !$v.title.required">Поле Тема не должно быть пустым</span>
-						<span class="helper-text invalid" v-if="submitted && !$v.title.maxLength">Поле Тема не должно быть более 255-ти знаков</span>
-					</div>
-					<div class="input-form">
-						<label for="text">* Текст письма</label>
-						<textarea name="text" placeholder="Текст письма" v-model="text"></textarea>
-						<span class="helper-text invalid" v-if="submitted && !$v.text.required">Поле Текст не должно быть пустым</span>
-					</div>
-					<div class="input-form">
-						<!--<button v-on:click="submitForm" type="submit">Отправить</button>-->
-						<button type="submit">Отправить</button>
-					</div>
-					<div class="message">Поля, отмеченные *, обязательны к заполнению.</div>
-					
+					<fieldset v-bind:disabled=ifFormSend>
+						<span class="send_message" v-if="send">Письмо успешно отправлено!</span>
+						<div class="input-form">
+							<input type="text" placeholder="* Email" v-model.trim="email">
+							<span class="helper-text invalid" v-if="!$v.email.required">Поле email не должно быть пустым</span>
+							<span class="helper-text invalid" v-if="!$v.email.email">Введите корректный email</span>
+						</div>
+						<div class="input-form">
+							<input type="text" placeholder="* Имя" v-model.trim="firstName">
+							<span class="helper-text invalid" v-if="!$v.firstName.required">Поле Имя не должно быть пустым</span>
+							<span class="helper-text invalid" v-if="!$v.firstName.minLength">Поле Имя не должно быть меньше 3-х знаков</span>
+						</div>
+						<div class="input-form">
+							<input type="text" placeholder="Отчество" v-model.trim="middleName">
+							<span class="helper-text invalid" v-if="!$v.middleName.maxLength">Поле Отчество не должно быть более 255-ти символов</span>
+						</div>
+						<div class="input-form">
+							<input type="text" placeholder="Фамилия" v-model.trim="lastName">
+							<span class="helper-text invalid" v-if="!$v.lastName.maxLength">Поле Фамилия не должно быть более 255-ти символов</span>
+						</div>
+						<div class="input-form">
+							<input type="text" placeholder="Телефон" v-model.trim="phoneNumber">
+							<span class="helper-text invalid" v-if="!$v.phoneNumber.minLength">Поле Телефон не должно быть меньше 11-ти цифр</span>
+							<span class="helper-text invalid" v-if="!$v.phoneNumber.maxLength">Поле Телефон не должно быть больше 11-ти цифр</span>
+						</div>
+
+						<div class="input-form">
+							<input type="text" placeholder="* Тема" v-model.trim="title">
+							<span class="helper-text invalid" v-if="!$v.title.required">Поле Тема не должно быть пустым</span>
+						</div>
+						<div class="input-form">
+							<textarea name="text" placeholder="* Текст письма" v-model="text"></textarea>
+							<span class="helper-text invalid" v-if="!$v.text.required">Поле Текст не должно быть пустым</span>
+							<span class="helper-text invalid" v-if="!$v.text.maxLength">Поле Текст не должно содержать более 2048 символов</span>
+						</div>
+						<div class="input-form">
+							<button type="submit">Отправить</button>
+
+						</div>
+						<div class="message">Поля, отмеченные *, обязательны к заполнению.</div>
+					</fieldset>
 				</form>
 		</div>
 		
@@ -88,6 +80,9 @@
 		background-color: #fff;
 		color:green;
 	}
+	fieldset {
+		border:0px;
+	}
 </style>
 
 <script>
@@ -97,63 +92,56 @@
 	Vue.use(Vuelidate)
 	import { required, minLength, maxLength, email, text, number, string } from "vuelidate/lib/validators"
 
-	//const regex_email = '/^[a-zA-Z0-9а-яА-Я._-&]+@([a-zA-Z0-9а-яА-Я._-&]++.[a-zA-Z | рф]2-4)+$/'
 	//const regex_email = '/^[a-zA-Z0-9._-&]+@([a-zA-Z0-9._-&]++.[a-zA-Z]2-5)+$/'
 	//const regex_phone = '/^(\d)11$/'
 	export default {
 		data: () => ({
-				email: '',
-				firstName: '',
-				lastName: '',
-				middleName: '',
-				title: '',
-				text: '',
-				phoneNumber: '',
-				submitted: false,
-				send: false
+			submitStatus: null,
+			send: false,
+			ifFormSend: false,
+			email: '',
+			firstName: '',
+			lastName: '',
+			middleName: '',
+			title: '',
+			text: '',
+			phoneNumber: '',
+			
 		}),
 		validations: {
 			email: {email, required, minLength:minLength(8),maxLength: maxLength(255)},
 			title: {string, required, minLength:minLength(4),maxLength: maxLength(255)},
 			firstName: {string, required, minLength:minLength(3),maxLength: maxLength(255)},
+			text: {required, minLength:minLength(50), maxLength: maxLength(2048), text},
 			lastName: {maxLength: maxLength(255), string},
 			middleName: {maxLength: maxLength(255), string},
-			text: {required, minLength:minLength(50), maxLength: maxLength(2048), text},
 			// правило не работает. не нашел пока нужное свойство для точного указания количества и в цифрах. Может регулярным выражением?
 			phoneNumber: { minLength: minLength(11), maxLength: maxLength(11), number},
 		},
 		methods: {
-			submitForm (event) {
-				this.submitted = true;
-				axios.post('https://test.cornapi.ru/feedback', {
-					firstName: this.firstName,
-					lastName: this.lastName,
-					middleName: this.middleName,
-					title: this.title,
-					email: this.email,
-					text: this.text,
-					phoneNumber: this.phoneNumber
-				}).then( res => {
-					this.send = true
-					//alert('Smth res ' + res)
-					// TODO очистка формы
-					event.target.reset()
-					this.email = ''
-					this.firstName = ''
-					this.lastName = ''
-					this.middleName = ''
-					this.title = ''
-					this.text = ''
-					this.phoneNumber = ''
-					this.submitted = false
-				}).catch( error => {
-					this.send = false
-				}).finally(() => {
-					return
-				})
-			}
+			async submitForm(event){
+					this.ifFormSend = true
+					let {email, firstName, lastName, middleName, title, text, phoneNumber} = this
+					let response = await axios.post('https://test.cornapi.ru/feedback', {email, firstName, lastName, middleName, title, text, phoneNumber
+					}).catch( error => {
+						//alert('error ' + error)
+						this.ifFormSend = false
+						return false
+					})
+					if (this.ifFormSend != false) {
+						console.log(response)
+						this.email = ''
+						this.firstName = ''
+						this.lastName = ''
+						this.middleName = ''
+						this.title = ''
+						this.text = ''
+						this.phoneNumber = ''
+						this.send = true
+						this.ifFormSend = false
+					}
+				}
 		}
 	}
-
 
 </script>
