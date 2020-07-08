95fb16f608167cfe30193ab364ca6e86e2a0744b
svoyparen <62475107+svoyparen@users.noreply.github.com>
2020-07-08 21:42:13 +0300

    Add files via upload

---
+++ components/FormInput.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="input-form">
+		<label for="" v-if="label !== null">
+			{{ label }}
+		</label>
+		<input
+			v-if="type === 'field'"
+			type="text"
+			:placeholder="placeholder"
+			v-model.trim="realValue"
+		>
+		<textarea
+			v-if="type === 'text'"
+			placeholder=""
+			v-model.trim="realValue"
+		></textarea>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			value: null,
+			label: { type: String, default: null },
+			type: { type: String, default: 'field' },
+		},
+
+		data: () => ({
+			realValue: null,
+		}),
+
+		watch: {
+			value(value) {
+				this.realValue = value
+			},
+
+			realValue(value) {
+				this.$emit('input', value)
+			},
+		},
+
+		beforeMount() {
+			this.realValue = this.value
+		}
+	}
+
+</script>
+
+<style>
+	
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,50 +3,46 @@
 		<div class="form">
 			<h1>Написать письмо</h1>
 				<form id="post_form" @submit.prevent="submitForm">
-					<fieldset v-bind:disabled=ifFormSend>
+					<fieldset v-bind:disabled=isFormSend>
 						<span class="send_message" v-if="send">Письмо успешно отправлено!</span>
-						<div class="input-form">
-							<input type="text" placeholder="* Email" v-model.trim="email">
-							<span class="helper-text invalid" v-if="!$v.email.required">Поле email не должно быть пустым</span>
-							<span class="helper-text invalid" v-if="!$v.email.email">Введите корректный email</span>
-						</div>
-						<div class="input-form">
-							<input type="text" placeholder="* Имя" v-model.trim="firstName">
-							<span class="helper-text invalid" v-if="!$v.firstName.required">Поле Имя не должно быть пустым</span>
-							<span class="helper-text invalid" v-if="!$v.firstName.minLength">Поле Имя не должно быть меньше 3-х знаков</span>
-						</div>
-						<div class="input-form">
-							<input type="text" placeholder="Отчество" v-model.trim="middleName">
-							<span class="helper-text invalid" v-if="!$v.middleName.maxLength">Поле Отчество не должно быть более 255-ти символов</span>
-						</div>
-						<div class="input-form">
-							<input type="text" placeholder="Фамилия" v-model.trim="lastName">
-							<span class="helper-text invalid" v-if="!$v.lastName.maxLength">Поле Фамилия не должно быть более 255-ти символов</span>
-						</div>
-						<div class="input-form">
-							<input type="text" placeholder="Телефон" v-model.trim="phoneNumber">
-							<span class="helper-text invalid" v-if="!$v.phoneNumber.minLength">Поле Телефон не должно быть меньше 11-ти цифр</span>
-							<span class="helper-text invalid" v-if="!$v.phoneNumber.maxLength">Поле Телефон не должно быть больше 11-ти цифр</span>
-						</div>
-
-						<div class="input-form">
-							<input type="text" placeholder="* Тема" v-model.trim="title">
-							<span class="helper-text invalid" v-if="!$v.title.required">Поле Тема не должно быть пустым</span>
-						</div>
-						<div class="input-form">
-							<textarea name="text" placeholder="* Текст письма" v-model="text"></textarea>
-							<span class="helper-text invalid" v-if="!$v.text.required">Поле Текст не должно быть пустым</span>
-							<span class="helper-text invalid" v-if="!$v.text.maxLength">Поле Текст не должно содержать более 2048 символов</span>
-						</div>
+						<FormInput
+							placeholder="Email"
+							v-model="email"
+						/>
+						<FormInput
+							placeholder="Имя"
+							v-model="firstName"
+						/>
+						<FormInput
+							placeholder="Отчество"
+							v-model="middleName"
+						/>
+						<FormInput
+							placeholder="Фамилия"
+							v-model="lastName"
+						/>
+						<FormInput
+							placeholder="Телефон"
+							v-model="phoneNumber"
+						/>
+						<FormInput
+							placeholder="Тема"
+							v-model="title"
+						/>
+						<FormInput
+							type="text"
+							placeholder="Текст письма"
+							v-model="text"
+						/>
 						<div class="input-form">
 							<button type="submit">Отправить</button>
 
 						</div>
-						<div class="message">Поля, отмеченные *, обязательны к заполнению.</div>
 					</fieldset>
 				</form>
 		</div>
 		
+<!--
 		<div id="contacts">
 			<p>
 				<strong>Адрес:</strong> перекресток Кибернетиков и Солдата Воскобойникова, 77а, оф.20<br />
@@ -56,8 +52,74 @@
 				<strong>Email: </strong>email@pisem.net<br />
 			</p>
 		</div>
-  </div>
+-->
+	</div>
 </template>
+
+<script>
+	import axios from "axios"
+	import Validate from 'vee-validate'
+	import { required, minLength, maxLength, email, text, number, string } from "vuelidate/lib/validators"
+	import FormInput from '~/components/FormInput'
+
+	export default {
+		components: {
+			FormInput,
+		},
+
+		data: () => ({
+			submitStatus: null,
+			send: false,
+			isFormSend: false,
+			email: '',
+			firstName: '',
+			lastName: '',
+			middleName: '',
+			title: '',
+			text: '',
+			phoneNumber: '',
+			
+		}),
+
+		validations: {
+			email: {email, required, minLength:minLength(8),maxLength: maxLength(255)},
+			title: {string, required, minLength:minLength(4),maxLength: maxLength(255)},
+			firstName: {string, required, minLength:minLength(3),maxLength: maxLength(255)},
+			text: {required, minLength:minLength(50), maxLength: maxLength(2048), text},
+			lastName: {maxLength: maxLength(255), string},
+			middleName: {maxLength: maxLength(255), string},
+			// правило не работает. не нашел пока нужное свойство для точного указания количества и в цифрах. Может регулярным выражением?
+			phoneNumber: { minLength: minLength(11), maxLength: maxLength(11), number},
+		},
+
+		methods: {
+			async submitForm(event){
+					this.isFormSend = true
+					
+					let {email, firstName, lastName, middleName, title, text, phoneNumber} = this
+					let response = await axios.post('https://test.cornapi.ru/feedback', {email, firstName, lastName, middleName, title, text, phoneNumber
+					}).catch( error => {
+						console.log( error )
+						this.isFormSend = false
+						return false
+					})
+					if (this.isFormSend !== false) {
+						console.log(response)
+						this.email = ''
+						this.firstName = ''
+						this.lastName = ''
+						this.middleName = ''
+						this.title = ''
+						this.text = ''
+						this.phoneNumber = ''
+						this.send = true
+						this.isFormSend = false
+					}
+				}
+		}
+	}
+
+</script>
 
 <style>
 	#post_form label {
@@ -84,64 +146,3 @@
 		border:0px;
 	}
 </style>
-
-<script>
-	import axios from "axios"
-	import Vue from 'vue'
-	import Vuelidate from 'vuelidate'
-	Vue.use(Vuelidate)
-	import { required, minLength, maxLength, email, text, number, string } from "vuelidate/lib/validators"
-
-	//const regex_email = '/^[a-zA-Z0-9._-&]+@([a-zA-Z0-9._-&]++.[a-zA-Z]2-5)+$/'
-	//const regex_phone = '/^(\d)11$/'
-	export default {
-		data: () => ({
-			submitStatus: null,
-			send: false,
-			ifFormSend: false,
-			email: '',
-			firstName: '',
-			lastName: '',
-			middleName: '',
-			title: '',
-			text: '',
-			phoneNumber: '',
-			
-		}),
-		validations: {
-			email: {email, required, minLength:minLength(8),maxLength: maxLength(255)},
-			title: {string, required, minLength:minLength(4),maxLength: maxLength(255)},
-			firstName: {string, required, minLength:minLength(3),maxLength: maxLength(255)},
-			text: {required, minLength:minLength(50), maxLength: maxLength(2048), text},
-			lastName: {maxLength: maxLength(255), string},
-			middleName: {maxLength: maxLength(255), string},
-			// правило не работает. не нашел пока нужное свойство для точного указания количества и в цифрах. Может регулярным выражением?
-			phoneNumber: { minLength: minLength(11), maxLength: maxLength(11), number},
-		},
-		methods: {
-			async submitForm(event){
-					this.ifFormSend = true
-					let {email, firstName, lastName, middleName, title, text, phoneNumber} = this
-					let response = await axios.post('https://test.cornapi.ru/feedback', {email, firstName, lastName, middleName, title, text, phoneNumber
-					}).catch( error => {
-						//alert('error ' + error)
-						this.ifFormSend = false
-						return false
-					})
-					if (this.ifFormSend != false) {
-						console.log(response)
-						this.email = ''
-						this.firstName = ''
-						this.lastName = ''
-						this.middleName = ''
-						this.title = ''
-						this.text = ''
-						this.phoneNumber = ''
-						this.send = true
-						this.ifFormSend = false
-					}
-				}
-		}
-	}
-
-</script>
