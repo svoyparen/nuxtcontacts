f86a7dc3ddc90115582b2fbbb5f117f5c5d53ceb
svoyparen <62475107+svoyparen@users.noreply.github.com>
2020-07-11 21:04:28 +0300

    Add files via upload

--- components/FormInput.vue
+++ components/FormInput.vue
@@ -1,17 +1,29 @@
 <template>
 	<div class="input-form">
-		<label for="" v-if="label !== null">
-			{{ label }}
+		<label v-if="label !== null">
+			{{ htmlLabel }}
 		</label>
 		<input
 			v-if="type === 'field'"
 			type="text"
-			:placeholder="placeholder"
+			:placeholder="htmlPlaceholder"
+			v-model.trim="realValue"
+		>
+		<input
+			v-else-if="type === 'email'"
+			type="email"
+			:placeholder="htmlPlaceholder"
+			v-model.trim="realValue"
+		>
+		<input
+			v-else-if="type === 'tel'"
+			type="tel"
+			:placeholder="htmlPlaceholder"
 			v-model.trim="realValue"
 		>
 		<textarea
-			v-if="type === 'text'"
-			:placeholder="placeholder"
+			v-else-if="type === 'text'"
+			:placeholder="htmlPlaceholder"
 			v-model.trim="realValue"
 		></textarea>
 	</div>
@@ -24,11 +36,35 @@
 			label: { type: String, default: null },
 			placeholder: { type: String, default: null },
 			type: { type: String, default: 'field' },
+			pattern: { type: String, default: null },
+			required: { type: Boolean, default: false },
+			count: { type: Number, default: 255 },
+			phoneLenght: { type: Number, default: 255 },
 		},
 
 		data: () => ({
 			realValue: null,
+			isValid: true,
 		}),
+
+		computed: {
+			htmlLabel() {
+				if ( this.required ) {
+					return `* ${this.label}`
+				}
+
+				return this.label
+			},
+		
+
+			htmlPlaceholder() {
+				if ( this.required ) {
+					return `* ${this.placeholder}`
+				}
+
+				return this.placeholder
+			},
+		},
 
 		watch: {
 			value(value) {
@@ -42,7 +78,23 @@
 
 		beforeMount() {
 			this.realValue = this.value
-		}
+		},
+
+		methods: {
+			validation() {
+				if( this.type === 'tel' ) {
+					//this.isValid = (!this.realValue) || (this.realValue.length == 11)
+					this.isValid = (this.realValue === null) || (this.realValue && this.realValue.length === 11)
+				}
+				else if ( this.type === 'email' ) {
+					this.isValid = this.realValue.length > 0 && this.realValue.length < this.count
+				}
+				else {
+					this.isValid = !this.required || (this.realValue && this.realValue.length > 0)
+				}
+				return this.isValid
+			},
+		},
 	}
 
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,19 @@
 		<div class="form">
 			<h1>Написать письмо</h1>
 				<form id="post_form" @submit.prevent="submitForm">
-					<fieldset v-bind:disabled=isFormSend>
+					<fieldset :disabled=isFormSend>
 						<span class="send_message" v-if="send">Письмо успешно отправлено!</span>
+						<span class="invalid" v-else>Не заполнены необходимые поля!</span>
 						<FormInput
+							type="email"
 							placeholder="Email"
 							v-model="email"
+							required
 						/>
 						<FormInput
 							placeholder="Имя"
 							v-model="firstName"
+							required
 						/>
 						<FormInput
 							placeholder="Отчество"
@@ -22,17 +26,21 @@
 							v-model="lastName"
 						/>
 						<FormInput
+							type="tel"
 							placeholder="Телефон"
 							v-model="phoneNumber"
+							pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
 						/>
 						<FormInput
 							placeholder="Тема"
 							v-model="title"
+							required
 						/>
 						<FormInput
 							type="text"
 							placeholder="Текст письма"
 							v-model="text"
+							required
 						/>
 						<div class="input-form">
 							<button type="submit">Отправить</button>
@@ -41,25 +49,11 @@
 					</fieldset>
 				</form>
 		</div>
-		
-<!--
-		<div id="contacts">
-			<p>
-				<strong>Адрес:</strong> перекресток Кибернетиков и Солдата Воскобойникова, 77а, оф.20<br />
-				<strong>Телефоны:</strong> 8-9999-322223<br />
-				8-0955-223322<br />
-				8-999-8888888<br />
-				<strong>Email: </strong>email@pisem.net<br />
-			</p>
-		</div>
--->
 	</div>
 </template>
 
 <script>
 	import axios from "axios"
-	import Validate from 'vee-validate'
-	import { required, minLength, maxLength, email, text, number, string } from "vuelidate/lib/validators"
 	import FormInput from '~/components/FormInput'
 
 	export default {
@@ -80,42 +74,30 @@
 			phoneNumber: '',
 			
 		}),
-
-		validations: {
-			email: {email, required, minLength:minLength(8),maxLength: maxLength(255)},
-			title: {string, required, minLength:minLength(4),maxLength: maxLength(255)},
-			firstName: {string, required, minLength:minLength(3),maxLength: maxLength(255)},
-			text: {required, minLength:minLength(50), maxLength: maxLength(2048), text},
-			lastName: {maxLength: maxLength(255), string},
-			middleName: {maxLength: maxLength(255), string},
-			// правило не работает. не нашел пока нужное свойство для точного указания количества и в цифрах. Может регулярным выражением?
-			phoneNumber: { minLength: minLength(11), maxLength: maxLength(11), number},
-		},
-
 		methods: {
 			async submitForm(event){
-					this.isFormSend = true
-					
-					let {email, firstName, lastName, middleName, title, text, phoneNumber} = this
-					let response = await axios.post('https://test.cornapi.ru/feedback', {email, firstName, lastName, middleName, title, text, phoneNumber
-					}).catch( error => {
-						console.log( error )
-						this.isFormSend = false
-						return false
-					})
-					if (this.isFormSend !== false) {
-						console.log(response)
-						this.email = ''
-						this.firstName = ''
-						this.lastName = ''
-						this.middleName = ''
-						this.title = ''
-						this.text = ''
-						this.phoneNumber = ''
-						this.send = true
-						this.isFormSend = false
-					}
+				this.isFormSend = true
+				
+				let {email, firstName, lastName, middleName, title, text, phoneNumber} = this
+				let response = await axios.post('https://test.cornapi.ru/feedback', {email, firstName, lastName, middleName, title, text, phoneNumber
+				}).catch( error => {
+					console.log( error )
+					this.isFormSend = false
+					return false
+				})
+				if (this.isFormSend !== false) {
+					console.log(response)
+					this.email = ''
+					this.firstName = ''
+					this.lastName = ''
+					this.middleName = ''
+					this.title = ''
+					this.text = ''
+					this.phoneNumber = ''
+					this.send = true
+					this.isFormSend = false
 				}
+			}
 		}
 	}
 
@@ -133,12 +115,16 @@
 		color: white;
 	}
 	.invalid {
+		margin:15px 40px;
+		padding: 5px;
+		font-size: 14px;
 		background-color: #fff;
 		color: red;
 	}
 	.send_message {
 		margin:15px 40px;
 		font-size: 14px;
+		padding: 5px;
 		background-color: #fff;
 		color:green;
 	}
