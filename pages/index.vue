fdaf2f05bb71562b49396819477ee310d7380c4f
svoyparen <62475107+svoyparen@users.noreply.github.com>
2020-07-15 23:28:01 +0300

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -11,57 +11,51 @@
 							v-model="email"
 							ref="email"
 							required
+							message = 'Wrong email'
 						/>
-						<span class="invalid" v-if="wrong_email" v-bind="email">{{ wrong_email }}</span>
 						<FormInput
-							type="field"
 							placeholder="Имя"
 							v-model="firstName"
 							ref="firstName"
 							required
+							message = 'Wrong first name'
 						/>
-						<span class="invalid" v-if="wrong_firstName" v-bind="firstName">{{ wrong_firstName }}</span>
 						<FormInput
-							type="field"
 							placeholder="Отчество"
 							v-model="middleName"
 							ref="middleName"
+							message = 'Wrong middle name'
 						/>
-						<span class="invalid" v-if="wrong_middleName" v-bind="middleName">{{ wrong_middleName }}</span>
 						<FormInput
-							type="field"
 							placeholder="Фамилия"
 							v-model="lastName"
 							ref="lastName"
+							message = 'Wrong last name'
 						/>
-						<span class="invalid" v-if="wrong_lastName" v-bind="lastName">{{ wrong_lastName }}</span>
 						<FormInput
 							type="tel"
 							placeholder="Телефон"
 							v-model="phoneNumber"
-							pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
 							ref="phoneNumber"
+							message = 'Wrong phone number'
 						/>
-						<span class="invalid" v-if="wrong_phoneNumber" v-bind="phoneNumber">{{ wrong_phoneNumber }}</span>
 						<FormInput
-							type="field"
 							placeholder="Тема"
 							v-model="title"
 							ref="title"
 							required
+							message = 'Wrong title'
 						/>
-						<span class="invalid" v-if="wrong_title" v-bind="title">{{ wrong_title }}</span>
 						<FormInput
 							type="text"
 							placeholder="Текст письма"
 							v-model="text"
 							ref="text"
+							message = 'Wrong text size'
 							required
 						/>
-						<span class="invalid" v-if="wrong_text" v-bind="text">{{ wrong_text }}</span>
 						<div class="input-form">
 							<button type="submit">Отправить</button>
-
 						</div>
 					</fieldset>
 				</form>
@@ -90,13 +84,6 @@
 			text: '',
 			phoneNumber: '',
 			errors: [],
-			wrong_email: '',
-			wrong_firstName: '',
-			wrong_middleName: '',
-			wrong_lastName: '',
-			wrong_phoneNumber: '',
-			wrong_title: '',
-			wrong_text: '',
 		}),
 		methods: {
 			async submitForm(event){
@@ -105,31 +92,24 @@
 
 				if(!this.$refs.email.validation()){
 					this.errors.push('Введен некорректный email')
-					this.wrong_email = 'Введен некорректный email'
 				}
 				if(!this.$refs.firstName.validation()){
 					this.errors.push('Не указано Имя')
-					this.wrong_firstName = 'Не указано Имя'
 				}
 				if(!this.$refs.middleName.validation()){
 					this.errors.push('Указано некорректное значение в поле Отчество')
-					this.wrong_middleName = 'Некорректное значение в поле Отчество'
 				}
 				if(!this.$refs.lastName.validation()){
 					this.errors.push('Указано некорректное значение в поле Фамилия')
-					this.wrong_lastName = 'Некорректное значение в поле Фамилия'
 				}
 				if(!this.$refs.phoneNumber.validation()){
 					this.errors.push('Введен некорректный номер телефона')
-					this.wrong_phoneNumber = 'Введен некорректный номер телефона'
 				}
 				if(!this.$refs.title.validation()){
 					this.errors.push('Не указана тема письма')
-					this.wrong_title = 'Не указана тема письма'
 				}
 				if(!this.$refs.text.validation()){
 					this.errors.push('Введено некорректное значение в поле Текст')
-					this.wrong_text = 'Введено некорректное значение в поле Текст'
 				}
 				if(this.errors.length > 0) {
 					console.log('There are some errors:\n' + this.errors)
@@ -138,12 +118,14 @@
 				}
 
 				let {email, firstName, lastName, middleName, title, text, phoneNumber} = this
+
 				let response = await axios.post('https://test.cornapi.ru/feedback', {email, firstName, lastName, middleName, title, text, phoneNumber
 				}).catch( error => {
 					console.log( error )
 					this.isFormSend = false
 					return false
 				})
+
 				if (this.isFormSend !== false) {
 					console.log(response)
 					this.email = ''
@@ -155,13 +137,6 @@
 					this.phoneNumber = ''
 					this.send = true
 					this.isFormSend = false
-					this.wrong_email = ''
-					this.wrong_firstName = ''
-					this.wrong_middleName = ''
-					this.wrong_lastName = ''
-					this.wrong_phoneNumber = ''
-					this.wrong_title = ''
-					this.wrong_text = ''
 				}
 			}
 		}
