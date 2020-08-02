b5e1f7b0e7b28193f0fb8c7dd7fd4178a2331054
svoyparen <62475107+svoyparen@users.noreply.github.com>
2020-08-02 21:46:09 +0300

    Add files via upload

---
+++ pages/Popup.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<PopupWindow
+			title="Заголовок модального окна"
+			message="Простое текстовое сообщение"
+			okTitle="Закрыть"
+		>
+		</PopupWindow>
+
+	</div>
+</template>
+
+<script>
+	import PopupWindow from '~/components/PopupWindow'
+
+	export default {
+		components: {
+			PopupWindow,
+		},
+
+	}
+</script>
+
+<style>
+	button {
+		margin-top: 20px;
+		color: #fff;
+		font-weight: bold;
+		text-transform: uppercase;
+		border: none;
+		background: #0fc3f5;
+		transition: .3s;
+	}
+</style>
