f70625476467c90d29e15070aaba097da0093157
svoyparen <62475107+svoyparen@users.noreply.github.com>
2020-08-02 21:45:40 +0300

    Add files via upload

---
+++ components/PopupWindow.vue
@@ -0,0 +1,84 @@
+<template>
+	<div class="PopupWindow">
+    <button @click="setIsVisible">Показать окно</button>
+    <div class="modal-mask" v-if="this.isVisisble">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <slot name="header" class="modal-header"><h3>{{ title }}</h3></slot>
+      		<slot name="content">{{ message }}</slot>
+      		<slot name="footer">
+      			<button type="button" @click="setIsVisible">
+      				{{ okTitle }}
+      			</button>
+      		</slot>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+	export default {
+		props: {
+			title: { type: String, default: 'Popup' },
+      message: {type: String, default: 'Message'},
+			okTitle: { type: String, default: 'Ok' },
+			loading: { type:Boolean, default: false },
+		},
+
+    data: ()=> ({
+      isVisisble: false,
+    }),
+
+    methods: {
+      setIsVisible() {
+        this.isVisisble = !this.isVisisble
+      }
+    }
+
+  }
+	
+</script>
+
+<style scoped>
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: table;
+  transition: opacity 0.9s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+</style>
