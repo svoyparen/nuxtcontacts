2aeec0f37c31eb036f9c8526c4dfe32dc220ad56
svoyparen <62475107+svoyparen@users.noreply.github.com>
2020-08-08 20:38:11 +0300

    Add files via upload

--- pages/Popup.vue
+++ pages/Popup.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-    <PopupWindow
-      okTitle="Закрыть"
-    >
-      <h2 slot="header">Заголовок модального окна</h2>
-      <p slot="content">Простое текстовое сообщение</p>
-
-    </PopupWindow>
-
+    <button @click="show">Показать окно</button>
+    <div >
+      <PopupWindow okTitle="Закрыть" v-if="this.isVisible">
+        <div slot="header" class="header">
+          <h4>Заголовок модального окна</h4>
+        </div>
+        <div slot="content">
+          <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil fugit nam eligendi maiores, soluta sit odit maxime doloribus adipisci dignissimos deleniti. Suscipit voluptatum, eos sit ea nam blanditiis minima ducimus!</span>
+          <span>Similique et aut non nemo accusantium soluta, a excepturi. Ab, rem. Incidunt perspiciatis assumenda, distinctio, ullam tenetur repellat delectus nisi corrupti corporis omnis saepe magni quam, esse velit beatae aspernatur.</span>
+          <span>Laudantium temporibus labore quae veritatis, ex nulla. Debitis illo illum dolorem. Vel quidem quaerat aperiam, praesentium ipsam at ex molestiae! Quaerat voluptatum quia quae accusamus vero perspiciatis quas earum odit.</span></p>
+        </div>
+        <div slot="footer">
+            <button type="button" @click="hide">
+              {{ okTitle }}
+            </button>
+        </div>
+      </PopupWindow>
+    </div>
   </div>
 </template>
 
@@ -17,6 +27,28 @@
     components: {
       PopupWindow,
     },
+
+    data() {
+      return {
+        isVisible: false,
+        okTitle: 'Close',
+      }
+    },
+
+    methods: {
+      show() {
+        this.isVisible = true
+        console.log(this.isVisible)
+      },
+
+      hide() {
+        if( this.isVisible == true ) {
+          this.isVisible = false
+          console.log(this.isVisible)
+        }
+      }
+
+    }
 
   }
 
@@ -32,10 +64,13 @@
     background: #0fc3f5;
     transition: .3s;
   }
-  h2 {
+  .header {
     margin: 0;
-    padding: 3px 15px;
+    padding: 5px 15px;
     background-color: #5533ff;
   }
-  
+  p {
+    padding: 5px 15px;
+  }
+
 </style>
---
+++ pages/Select.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<DropList
+			label=""
+			placeholder=""
+			value=""
+			options=""
+			disabled
+			required
+		/>
+	</div>	
+</template>
+
+<script>
+	import DropList from '~/components/DropList'
+	export default {
+		components: {
+			DropList,
+		},
+/*
+		data: () => {
+			label:'Title',
+			placeholder:'Something here',
+			value:2,
+			options:{
+			},
+		}
+*/
+	}	
+</script>
